6210a659e5406d7fbc57eb72e27e2980ea7fccd7
eawane <eawane@ivytech.edu>
2026-04-20 21:58:44 -0400

    Create Loan.java

---
+++ src/main/java/com/example/loancalculator/Loan.java
@@ -0,0 +1,27 @@
+package com.example.loancalculator;
+
+import java.util.Date;
+
+public class Loan {
+    private double annualInterestRate;
+    private int numberOfYears;
+    private double loanAmount;
+    private Date loanDate;
+
+    public Loan(double annualInterestRate, int numberOfYears, double loanAmount) {
+        this.annualInterestRate = annualInterestRate;
+        this.numberOfYears = numberOfYears;
+        this.loanAmount = loanAmount;
+        this.loanDate = new Date();
+    }
+
+    public double getMonthlyPayment() {
+        double monthlyInterestRate = annualInterestRate / 1200;
+        return loanAmount * monthlyInterestRate /
+                (1 - (1 / Math.pow(1 + monthlyInterestRate, numberOfYears * 12)));
+    }
+
+    public double getTotalPayment() {
+        return getMonthlyPayment() * numberOfYears * 12;
+    }
+}
